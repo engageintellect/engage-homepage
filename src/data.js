1fe6eadda0277ad6ee83b18e7b5e4b1342f21ab0
engageintellect <jc9361@gmail.com>
2023-02-20 13:23:24 -0800

    cleaning up footer

--- src/data.js
+++ src/data.js
@@ -64,8 +64,13 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/engageintellect' },
   ],
+  // footNote: `
+  //   <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
+  //   Made by <a class="text-blue-600 hover:underline dark:text-stone-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  // `,
+
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-stone-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm"></span>
+    Made by <a class="text-blue-600 hover:underline dark:text-stone-200" href="https://github.com/engageintllect">@engageintellect</a> · All rights reserved.
   `,
 };
